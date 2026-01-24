f51a229a8d01c274683794b4567d208ef10164ff
yousefahmedclaro <yousef.ahmed@claro.eg>
2026-01-24 18:40:17 +0200

    Add Abstraction on IRequest, IRequestHandler interfaces

---
+++ src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductHandler.cs
@@ -0,0 +1,18 @@
+﻿using BuildingBlocks.CQRS;
+using MediatR;
+
+namespace Catalog.API.Products.CreateProduct
+{
+    public record CreateProductCommand(string Name, string Description, decimal Price, string ImageFile)
+        : ICommand<CreateProductResult>;
+
+    public record CreateProductResult(Guid Id);
+
+    public class CreateProductHandler : ICommandHandler<CreateProductCommand, CreateProductResult>
+    {
+        public Task<CreateProductResult> Handle(CreateProductCommand command, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
