dbafcf280539994863cf624126cdae7b88e4bead
yousefahmedclaro <yousef.ahmed@claro.eg>
2026-03-25 02:20:44 +0200

    feat/ update product command handler and minimal endpoint

--- src/Services/Catalog/Catalog.API/Products/UpdateProduct/UpdateProductEndPoint.cs
+++ src/Services/Catalog/Catalog.API/Products/UpdateProduct/UpdateProductEndPoint.cs
@@ -1,6 +1,24 @@
 ﻿namespace Catalog.API.Products.UpdateProduct
 {
-    public class UpdateProductEndPoint
+    public record UpdateProductRequest(Guid Id, string Name, string Description, string ImageFile , decimal Price, List<string> Category)
+        : ICommand<UpdateProductResponse>;
+
+    public record UpdateProductResponse(bool IsSuccess);
+
+    public class UpdateProductEndPoint : ICarterModule
     {
+        public void AddRoutes(IEndpointRouteBuilder app)
+        {
+            app.MapPut("Products", async (UpdateProductRequest request, ISender sender) =>
+            {
+                var command = request.Adapt<UpdateProductCommand>();
+
+                var result = await sender.Send(command);
+
+                var response = result.Adapt<UpdateProductResponse>();
+
+                return Results.Ok(response);
+            });
+        }
     }
 }
--- src/Services/Catalog/Catalog.API/Products/UpdateProduct/UpdateProductHandler.cs
+++ src/Services/Catalog/Catalog.API/Products/UpdateProduct/UpdateProductHandler.cs
@@ -1,6 +1,36 @@
 ﻿namespace Catalog.API.Products.UpdateProduct
 {
-    public class UpdateProductHandler
+    public record UpdateProductCommand(Guid Id, string Name, string Description, string ImageFile, decimal Price, List<string> Category)
+        : ICommand<UpdateProductResult>;
+
+    public record UpdateProductResult(bool IsSuccess);
+
+    public class UpdateProductCommandHandler
+        (IDocumentSession session, ILogger<UpdateProductCommandHandler> logger)
+        : ICommandHandler<UpdateProductCommand, UpdateProductResult>
     {
+        public async Task<UpdateProductResult> Handle(UpdateProductCommand command, CancellationToken cancellationToken)
+        {
+            logger.LogInformation("Handling UpdateProductCommand for Id: {Id}", command.Id);
+
+            var product = await session.LoadAsync<Product>(command.Id, cancellationToken);
+
+            if (product is null)
+            {
+                throw new ProductNotFoundException();
+            }
+
+            product.Name = command.Name;
+            product.Description = command.Description;
+            product.Price = command.Price;
+            product.ImageFile = command.ImageFile;
+            product.Category = command.Category;
+
+            session.Update(product);
+
+            await session.SaveChangesAsync();
+
+            return new UpdateProductResult(true);
+        }
     }
 }
