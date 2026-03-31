5b18417707a7cfb0001832afbb839230adf23662
yousefahmedclaro <yousef.ahmed@claro.eg>
2026-03-31 03:57:59 +0200

    feat/ Exception Handling in readable way

--- src/Services/Catalog/Catalog.API/Program.cs
+++ src/Services/Catalog/Catalog.API/Program.cs
@@ -1,5 +1,7 @@
 using BuildingBlocks.Behaviors;
 using FluentValidation;
+using Microsoft.AspNetCore.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -24,7 +26,33 @@ builder.Services.AddMarten(opt =>
 var app = builder.Build();
 
 
+app.UseExceptionHandler(exceptionHandlerApp =>
+{
+    exceptionHandlerApp.Run(async context =>
+    {
+        var exception = context.Features.Get<IExceptionHandlerFeature>()?.Error;
 
+        if (exception is null) return;
+
+        var problemDetails = new ProblemDetails()
+        {
+            Title = exception.Message,
+            Status = StatusCodes.Status500InternalServerError,
+            Detail = exception.StackTrace
+        };
+
+        var logger = app.Services.GetRequiredService<ILogger<Program>>();
+
+        logger.LogError(exception, exception.Message);
+
+        context.Response.StatusCode = StatusCodes.Status500InternalServerError;
+
+        context.Response.ContentType = "application/json";
+
+        await context.Response.WriteAsJsonAsync(problemDetails);
+
+    });
+});
 
 
 
