c070aea81d6e78272477cd2eeb69e1f1f55796bd
yousefahmedclaro <yousef.ahmed@claro.eg>
2026-04-01 01:02:50 +0200

    feat/ dedvelop Delete basket handler and endpoint

--- src/Services/Basket/Basket.API/Basket/DeleteBasket/DeleteBasketEndpoint.cs
+++ src/Services/Basket/Basket.API/Basket/DeleteBasket/DeleteBasketEndpoint.cs
@@ -1,6 +1,26 @@
-﻿namespace Basket.API.Basket.DeleteBasket
+﻿
+namespace Basket.API.Basket.DeleteBasket
 {
-    public class DeleteBasketEndpoint
+    public record DeleteBasketRequest(string UserName);
+
+    public record DeleteBasketResponse(bool IsSuccess);
+
+    public class DeleteBasketEndpoint : ICarterModule
     {
+        public void AddRoutes(IEndpointRouteBuilder app)
+        {
+            app.MapDelete("basket/{UserName}", async (string UserName, ISender sender) =>
+            {
+
+                // 1. send the command to mediator for handling
+                var result = await sender.Send(new DeleteBasketCommand(UserName));
+
+                // 2. adapt the result to response object
+                var response = result.Adapt<DeleteBasketResponse>();
+
+                // 3. return the response
+                return Results.Ok(response);
+            });
+        }
     }
 }
