99c406be9a1db27ab819012a7b9b56239330ff75
yousefahmedclaro <yousef.ahmed@claro.eg>
2026-04-02 02:27:09 +0200

    feat/ implement Basket repo using Marten IDocument session

---
+++ src/Services/Basket/Basket.API/Exception/BasketNotFound.cs
@@ -0,0 +1,10 @@
+﻿
+namespace Basket.API.Exception
+{
+    public class BasketNotFound : NotFoundException
+    {
+        public BasketNotFound(string userName) : base($"Basket found.",userName)
+        {
+        }
+    }
+}
