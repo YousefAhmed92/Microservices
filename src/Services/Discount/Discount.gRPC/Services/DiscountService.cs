0252806e92eb2de7bdd1b4589ff87899b160a8a6
yousefahmedclaro <yousef.ahmed@claro.eg>
2026-04-06 01:50:35 +0200

    feat/ develop Discount get method

--- src/Services/Discount/Discount.gRPC/Services/DiscountService.cs
+++ src/Services/Discount/Discount.gRPC/Services/DiscountService.cs
@@ -1,12 +1,32 @@
-﻿using Grpc.Core;
+﻿using Discount.gRPC.Data;
+using Grpc.Core;
+using Mapster;
+using Microsoft.EntityFrameworkCore;
 
 namespace Discount.gRPC.Services
 {
-    public class DiscountService : DiscountProtoService.DiscountProtoServiceBase
+    public class DiscountService (DiscountDbContext dbcontext, ILogger<DiscountService> logger)
+        : DiscountProtoService.DiscountProtoServiceBase
     {
-        public override Task<CouponModel> GetDiscount(GetDiscountRequest request, ServerCallContext context)
+        public override async Task<CouponModel> GetDiscount(GetDiscountRequest request, ServerCallContext context)
         {
-            return base.GetDiscount(request, context);
+            var Coupon = await dbcontext.Coupons.FirstOrDefaultAsync(x => x.ProductName == request.ProductName);
+
+            if (Coupon == null)
+            {
+                Coupon = new Models.Coupon
+                {
+                    ProductName = "No Discount",
+                    Amount = 0,
+                    Description = "No Discount"
+                };
+            }
+
+            logger.LogInformation("Discount is retrieved for ProductName : {ProductName}, Amount : {Amount}", Coupon.ProductName, Coupon.Amount);
+
+            var couponModel = Coupon.Adapt<CouponModel>();
+
+            return couponModel;
         }
 
         public override Task<CouponModel> CreateDiscount(CreateDiscountRequest request, ServerCallContext context)
