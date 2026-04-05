07f4a8f456f680f91a794ee9808ea7f09fc2e6bb
yousefahmedclaro <yousef.ahmed@claro.eg>
2026-04-06 01:12:35 +0200

    Create Discount DB context with sqllite DB and migration

---
+++ src/Services/Discount/Discount.gRPC/Data/DiscountDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Discount.gRPC.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Discount.gRPC.Data
+{
+    public class DiscountDbContext : DbContext
+    {
+        public DiscountDbContext(DbContextOptions<DiscountDbContext> context) : base(context)
+        {
+            
+        }
+
+        public DbSet<Coupon> Coupons { get; set; }
+    }
+}
--- src/Services/Discount/Discount.gRPC/Program.cs
+++ src/Services/Discount/Discount.gRPC/Program.cs
@@ -1,9 +1,16 @@
+using Discount.gRPC.Data;
 using Discount.gRPC.Services;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddGrpc();
+
+builder.Services.AddDbContext<DiscountDbContext>(opt =>
+{
+    opt.UseSqlite(builder.Configuration.GetConnectionString("Database")!);
+});
 
 var app = builder.Build();
 
