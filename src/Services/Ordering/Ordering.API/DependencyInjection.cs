50911b8d7d25c0865e994bb9d9a248f5f50a8c31
yousefahmedclaro <yousef.ahmed@claro.eg>
2026-04-10 01:17:23 +0200

    Create Ordering Service with setup the clean Architecture, references and Dependency Injection

---
+++ src/Services/Ordering/Ordering.API/DependencyInjection.cs
@@ -0,0 +1,15 @@
+﻿namespace Ordering.API
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddApiServices(this IServiceCollection services)
+        {
+            return services;
+        }
+
+        public static WebApplicationBuilder AddApiServices(this WebApplicationBuilder app)
+        {
+            return app;
+        }
+    }
+}
