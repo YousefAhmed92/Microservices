a34a43452245a04a822da7ab0b0b3879733c082b
yousefahmedclaro <yousef.ahmed@claro.eg>
2026-04-09 17:07:36 +0200

    feat: Consume Discount.gRPC from basket service when adding a cart

--- src/Services/Basket/Basket.API/Basket/StoreBasket/StoreBasketHandler.cs
+++ src/Services/Basket/Basket.API/Basket/StoreBasket/StoreBasketHandler.cs
@@ -1,4 +1,6 @@
-﻿namespace Basket.API.Basket.StoreBasket
+﻿using Discount.gRPC;
+
+namespace Basket.API.Basket.StoreBasket
 {
     public record StoreBasketCommand(ShoppingCart ShoppingCart): ICommand<StoreBasketResult>;
 
@@ -18,14 +20,26 @@
         }
     }
 
-    public class StoreBasketCommandHandler (IBasketRepository repository)
+    public class StoreBasketCommandHandler 
+        (IBasketRepository repository, DiscountProtoService.DiscountProtoServiceClient discount)
         : ICommandHandler<StoreBasketCommand, StoreBasketResult>
     {
         public async Task<StoreBasketResult> Handle(StoreBasketCommand command, CancellationToken cancellationToken)
         {
+            await DeductDiscount(command.ShoppingCart, cancellationToken);
+
             await repository.StoreBasket(command.ShoppingCart, cancellationToken);
 
             return new StoreBasketResult(command.ShoppingCart.UserName);
+        }
+
+        private async Task DeductDiscount(ShoppingCart cart, CancellationToken cancellationToken)
+        {
+            foreach (var item in cart.Items)
+            {
+                var copun = await discount.GetDiscountAsync(new GetDiscountRequest { ProductName = item.ProductName }, cancellationToken: cancellationToken);
+                item.Price -= copun.Amount;
+            }
         }
     }
 }
