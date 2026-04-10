4cc007a4db1d0f8b720a967f98a2415b2c494928
yousefahmedclaro <yousef.ahmed@claro.eg>
2026-04-10 16:42:09 +0200

    feat: develop Abstractions in domain layer for (Entity, Domain Events, aggregate)

---
+++ src/Services/Ordering/Ordering.Domain/Abstractions/IDomainEvent.cs
@@ -0,0 +1,14 @@
+﻿using MediatR;
+using System.Reflection;
+
+namespace Ordering.Domain.Abstractions
+{
+    public interface IDomainEvent : INotification
+    {
+        public Guid EventId => Guid.NewGuid();
+
+        public DateTime OccurredOn { get; }
+
+        public string EventType => GetType().AssemblyQualifiedName;
+    }
+}
