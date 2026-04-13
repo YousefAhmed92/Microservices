2434f55d1e43c9e5dac24d1cfd87dbefdba148de
yousefahmedclaro <yousef.ahmed@claro.eg>
2026-04-14 00:34:25 +0200

    Feat: Develop delete order command and handler

---
+++ src/Services/Ordering/Ordering.Application/Orders/Commands/DeleteOrder/DeleteOrderCommand.cs
@@ -0,0 +1,17 @@
+﻿namespace Ordering.Application.Orders.Commands.DeleteOrder
+{
+    public record DeleteOrderCommand(Guid OrderID)
+        : ICommand<DeleteOrderResult>;
+
+    public record DeleteOrderResult(bool IsSuccess);
+
+    public class DeleteOrderCommandValidator : AbstractValidator<DeleteOrderCommand>
+    {
+        public DeleteOrderCommandValidator()
+        {
+            RuleFor(x => x.OrderID)
+                .NotEmpty()
+                .WithMessage("Order ID cannot be empty.");
+        }
+    }
+}
---
+++ src/Services/Ordering/Ordering.Application/Orders/Commands/DeleteOrder/DeleteOrderHandler.cs
@@ -0,0 +1,25 @@
+﻿namespace Ordering.Application.Orders.Commands.DeleteOrder
+{
+    public class DeleteOrderHandler(IApplicationDbContext context)
+        : ICommandHandler<DeleteOrderCommand, DeleteOrderResult>
+    {
+        public async Task<DeleteOrderResult> Handle(DeleteOrderCommand command, CancellationToken cancellationToken)
+        {
+            var orderId = OrderId.Of(command.OrderID);
+
+            var order = await context.orders.
+                FindAsync(new object[] { orderId }, cancellationToken);
+
+            if (order is null)
+            {
+                throw new OrderNotFoundException(command.OrderID);
+            }
+
+            context.orders.Remove(order);
+
+            await context.SaveChangesAsync(cancellationToken);
+
+            return new DeleteOrderResult(true);
+        }
+    }
+}
