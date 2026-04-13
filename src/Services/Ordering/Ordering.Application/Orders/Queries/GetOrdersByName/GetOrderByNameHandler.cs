0e5522f0af8ab49348d9b647008fb64e83e5f512
yousefahmedclaro <yousef.ahmed@claro.eg>
2026-04-14 01:54:10 +0200

    fear: Develop get order by name query with extension method to convert order into dto list

---
+++ src/Services/Ordering/Ordering.Application/Orders/Queries/GetOrdersByName/GetOrderByNameHandler.cs
@@ -0,0 +1,22 @@
+﻿using Ordering.Application.Extensions;
+
+namespace Ordering.Application.Orders.Queries.GetOrdersByName
+{
+    public class GetOrderByNameHandler(IApplicationDbContext context)
+        : IQueryHandler<GetOrderByNameQuery, GetOrdersByNameResult>
+    {
+        public async Task<GetOrdersByNameResult> Handle(GetOrderByNameQuery query, CancellationToken cancellationToken)
+        {
+            var orders = await context.orders
+                .Include(x => x.OrderItems)
+                .AsNoTracking()
+                .Where(x => x.OrderName.Value.Contains(query.name))
+                .OrderBy(x => x.OrderName)
+                .ToListAsync();
+
+
+            return new GetOrdersByNameResult(orders.ToOrderDtoList());
+        }
+
+    }
+}
---
+++ src/Services/Ordering/Ordering.Application/Orders/Queries/GetOrdersByName/GetOrderByNameQuery.cs
@@ -0,0 +1,7 @@
+﻿namespace Ordering.Application.Orders.Queries.GetOrdersByName
+{
+    public record GetOrderByNameQuery(string name)
+        : IQuery<GetOrdersByNameResult>;
+
+    public record GetOrdersByNameResult(IEnumerable<OrderDto> Orders);
+}
