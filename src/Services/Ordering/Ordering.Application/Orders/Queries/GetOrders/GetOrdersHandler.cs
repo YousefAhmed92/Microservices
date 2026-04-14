d02d7ded5da95de245aa3021818cc285f3f14407
yousefahmedclaro <yousef.ahmed@claro.eg>
2026-04-14 02:21:31 +0200

    feat: create pagination request and result classes in shared project then using them in new get orders handler

---
+++ src/Services/Ordering/Ordering.Application/Orders/Queries/GetOrders/GetOrdersHandler.cs
@@ -0,0 +1,28 @@
+﻿using BuildingBlocks.Pagination;
+
+namespace Ordering.Application.Orders.Queries.GetOrders
+{
+    public class GetOrdersHandler(IApplicationDbContext context)
+        : IQueryHandler<GetOrdersQuery, GetOrdersResult>
+    {
+        public async Task<GetOrdersResult> Handle(GetOrdersQuery query, CancellationToken cancellationToken)
+        {
+            var pageIndex= query.PaginationRequest.pageIndex;
+
+            var pageSize = query.PaginationRequest.pageSize;
+
+            var totalCount = await context.orders.LongCountAsync(cancellationToken);
+
+            var orders = await context.orders
+                .Include(x => x.OrderItems)
+                .OrderBy(x => x.OrderName.Value)
+                .Skip(pageIndex * pageSize)
+                .Take(pageSize)
+                .ToListAsync(cancellationToken);
+
+            return new GetOrdersResult(
+                new PaginationResult<OrderDto>
+                (pageIndex, pageSize, totalCount, orders.ToOrderDtoList()));
+        }
+    }
+}
---
+++ src/Services/Ordering/Ordering.Application/Orders/Queries/GetOrders/GetOrdersQuery.cs
@@ -0,0 +1,9 @@
+﻿using BuildingBlocks.Pagination;
+
+namespace Ordering.Application.Orders.Queries.GetOrders
+{
+    public record GetOrdersQuery(PaginationRequest PaginationRequest)
+        : IQuery<GetOrdersResult>;
+
+    public record GetOrdersResult(PaginationResult<OrderDto> Orders);
+}
