c63221ff1a76dbd19fc5fdd8ab46f4a341bf96b0
yousefahmedclaro <yousef.ahmed@claro.eg>
2026-04-14 02:00:43 +0200

    feat: develop get order by customer query and handler

---
+++ src/Services/Ordering/Ordering.Application/Orders/Queries/GetOrderByCustomer/GetOrderByCustomerHandler.cs
@@ -0,0 +1,18 @@
+﻿namespace Ordering.Application.Orders.Queries.GetOrderByCustomer
+{
+    public class GetOrderByCustomerHandler(IApplicationDbContext context)
+        : IQueryHandler<GetOrderByCustomerQuery, GetOrderByCustomerResult>
+    {
+        public async Task<GetOrderByCustomerResult> Handle(GetOrderByCustomerQuery query, CancellationToken cancellationToken)
+        {
+            var orders = await context.orders
+                .Include(x => x.OrderItems)
+                .AsNoTracking()
+                .Where(x => x.CustomerId == CustomerId.Of(query.CustomerId))
+                .OrderBy(x => x.OrderName.Value)
+                .ToListAsync(cancellationToken);
+
+            return new GetOrderByCustomerResult(orders.ToOrderDtoList());
+        }
+    }
+}
---
+++ src/Services/Ordering/Ordering.Application/Orders/Queries/GetOrderByCustomer/GetOrderByCustomerQuery.cs
@@ -0,0 +1,7 @@
+﻿namespace Ordering.Application.Orders.Queries.GetOrderByCustomer
+{
+    public record GetOrderByCustomerQuery(Guid CustomerId)
+        : IQuery<GetOrderByCustomerResult>;
+
+    public record GetOrderByCustomerResult(IEnumerable<OrderDto> Orders);
+}
