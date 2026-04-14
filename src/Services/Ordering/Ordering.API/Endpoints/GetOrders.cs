d6ccbe46ed0cbbb0b5003c8ced6b0d2b73a4bcbb
yousefahmedclaro <yousef.ahmed@claro.eg>
2026-04-14 17:08:21 +0200

    feat: develop Get Paginated Orders endpoint

--- src/Services/Ordering/Ordering.API/Endpoints/GetOrders.cs
+++ src/Services/Ordering/Ordering.API/Endpoints/GetOrders.cs
@@ -1,6 +1,22 @@
-﻿namespace Ordering.API.Endpoints
+﻿using BuildingBlocks.Pagination;
+using Ordering.Application.Orders.Queries.GetOrders;
+
+namespace Ordering.API.Endpoints
 {
-    public class GetOrders
+    public record GetOrdersResponse(PaginationResult<OrderDto> Orders);
+
+    public class GetOrders : ICarterModule
     {
+        public void AddRoutes(IEndpointRouteBuilder app)
+        {
+            app.MapGet("/orders", async ([AsParameters] PaginationRequest request, ISender sender) =>
+            {
+                var result = await sender.Send(new GetOrdersQuery(request));
+
+                var response = result.Adapt<GetOrdersResponse>();
+
+                return Results.Ok(response);
+            });
+        }
     }
 }
