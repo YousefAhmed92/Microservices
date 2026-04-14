a65a344a11dedd2df1ee2c32c9fbc48d07972392
yousefahmedclaro <yousef.ahmed@claro.eg>
2026-04-14 03:35:05 +0200

    feat: Register carter in the presentation layer and Develop Create Order minimal endpoint

---
+++ src/Services/Ordering/Ordering.API/Endpoints/CreateOrder.cs
@@ -0,0 +1,28 @@
+﻿using Ordering.Application.Orders.Commands.CreateOrder;
+
+namespace Ordering.API.Endpoints
+{
+    public record CreateOrderRequest(OrderDto Order);
+
+    public record CreateOrderResponse(Guid Id);
+
+    public class CreateOrder : ICarterModule
+    {
+        public void AddRoutes(IEndpointRouteBuilder app)
+        {
+            app.MapPost("/orders", async (CreateOrderRequest request, ISender sender) =>
+            {
+                // map the incoming request into a command
+                var command = request.Adapt<CreateOrderCommand>();
+
+                // use the mediator to send the command to the  handler
+                var result = await sender.Send(command);
+
+                // map the result into a response
+                var response = result.Adapt<CreateOrderResponse>();
+
+                return Results.Created($"/orders/{response.Id}", response);
+            });
+        }
+    }
+}
