fbee9d9955bd352e5fa6496ca1576fdeaebdd59e
yousefahmedclaro <yousef.ahmed@claro.eg>
2026-04-20 17:15:25 +0200

    Feat: publish basket checkout event in basket layer

---
+++ src/Services/Basket/Basket.API/Basket/CheckoutBasket/CheckoutBasketHandler.cs
@@ -0,0 +1,50 @@
+﻿using Basket.API.Dtos;
+using BuildingBlocksMessaging.Events;
+using MassTransit;
+
+namespace Basket.API.Basket.CheckoutBasket
+{
+    public record CheckoutBasketCommand(BastetCheckoutDto BastetCheckoutDto)
+    : ICommand<CheckoutBasketResult>;
+
+    public record CheckoutBasketResult(bool IsSuccess);
+
+    public class BasketCheckoutValidator : AbstractValidator<CheckoutBasketCommand>
+    {
+        public BasketCheckoutValidator()
+        {
+            RuleFor(x => x.BastetCheckoutDto)
+                .NotNull()
+                .WithMessage("checkout information is required");
+
+            RuleFor(x => x.BastetCheckoutDto.UserName)
+                .NotEmpty()
+                .WithMessage("username is required");
+        }
+    }
+
+    public class CheckoutBasketCommandHandler
+        (IBasketRepository basketRepository, IPublishEndpoint publishEndPoint)
+        : ICommandHandler<CheckoutBasketCommand, CheckoutBasketResult>
+    {
+        public async Task<CheckoutBasketResult> Handle(CheckoutBasketCommand command, CancellationToken cancellationToken)
+        {
+            var basket = await basketRepository.GetBasket(command.BastetCheckoutDto.UserName);
+
+            if (basket is null)
+            {
+                return new CheckoutBasketResult(false);
+            }
+
+            var eventMessage = command.BastetCheckoutDto.Adapt<BasketCheckoutEvent>();
+
+            eventMessage.TotalPrice = basket.TotalPrice;
+
+            await publishEndPoint.Publish(eventMessage, cancellationToken);
+
+            await basketRepository.DeleteBasket(command.BastetCheckoutDto.UserName, cancellationToken);
+
+            return new CheckoutBasketResult(true);
+        }
+    }
+}
--- src/Services/Basket/Basket.API/Program.cs
+++ src/Services/Basket/Basket.API/Program.cs
@@ -1,4 +1,5 @@
 using BuildingBlocks.Exceptions.Handler;
+using BuildingBlocksMessaging.MassTransit;
 using Discount.gRPC;
 using Microsoft.AspNetCore.Diagnostics.HealthChecks;
 
@@ -46,6 +47,8 @@ builder.Services.AddGrpcClient<DiscountProtoService.DiscountProtoServiceClient>(
     };
     return handler;
 });
+
+builder.Services.AddMessageBroker(builder.Configuration);
 
 builder.Services.AddExceptionHandler<CustomExceptionHandler>();
 
